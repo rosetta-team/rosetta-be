5d919ff128c9e8a5bdada54f17f8ad8a5dcf4ab8
msimon42 <msimon@uab.edu>
2020-04-02 20:44:57 -0600

    Add root route

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config = True)
-     app.config.from_mapping(
+    app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
@@ -17,3 +17,9 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @app.route('/')
+    def route():
+        return 'Test'
+
+    return app
