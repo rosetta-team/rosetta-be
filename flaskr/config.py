ff16546bbab1faa4ee44f27995625207e7c2dc91
msimon42 <msimon@uab.edu>
2020-04-05 11:52:48 -0600

    Update database uri

--- flaskr/config.py
+++ flaskr/config.py
@@ -5,7 +5,8 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/rosetta_dev"
+
 
 
 class ProductionConfig(Config):
