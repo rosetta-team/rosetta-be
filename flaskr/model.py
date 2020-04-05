569734ad751de23b89c8582c4c9db31050568586
msimon42 <msimon@uab.edu>
2020-04-05 11:52:20 -0600

    Update configs

--- flaskr/model.py
+++ flaskr/model.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from app import db
 
-db = SQLAlchemy()
+# db = create_app()
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/rosetta_dev"
+
 class Language(db.Model):
+    __tablename__ = 'languages'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String())
+
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
         return '<Language %r>' % self.name
