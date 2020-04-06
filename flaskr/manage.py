7bcbcb2359e5a8797f962885551ace05046c7483
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-06 16:50:22 -0600

    Add definition of shell context with ORM access

--- flaskr/manage.py
+++ flaskr/manage.py
@@ -1,14 +1,20 @@
-
-from flask_script import Manager
+# Import packages
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
-from .app import app, db
+# Import application, database, and models
+from app import app, db, Language, Method
 
+# Instantiate Migrate and Manager
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+# Add command line arguments for database and shell with ORM context
 manager.add_command('db', MigrateCommand)
-
+def make_shell_context():
+    return dict(app=app, db=db, Language=Language, Method=Method)
+manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
     manager.run()
