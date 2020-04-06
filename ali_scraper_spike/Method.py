486b0f5e1c781f690673ddcb007293ad6915cf3b
Alison Vermeil <alison.minton@gmail.com>
2020-04-06 12:53:14 -0600

    Add url to method

--- ali_scraper_spike/Method.py
+++ ali_scraper_spike/Method.py
@@ -1,7 +1,8 @@
 class Method:
 
-    def __init__(self, name, call_seq, description, snippet):
+    def __init__(self, name, url, call_seq, description, snippet):
         self.name = name
+        self.url = url
         self.call_seq = call_seq
         self.description = description
         self.snippet = snippet
