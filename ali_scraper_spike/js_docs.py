32bfe7ddbc84f329c1776cf1c7b785c4654d9ae3
Alison Vermeil <alison.minton@gmail.com>
2020-04-06 13:41:38 -0600

    JS complete, wip

--- ali_scraper_spike/js_docs.py
+++ ali_scraper_spike/js_docs.py
@@ -19,33 +19,51 @@ for link in method_links[6:45]:
     description = link['title']
     if "deprecated" not in description:
         name = link.find('code').text
+        print(name)
         url = 'https://developer.mozilla.org/' + link['href']
         method_page = requests.get(url) #have to make another request to the new site
         method_soup = BeautifulSoup(method_page.content, 'html.parser')
-        # import code; code.interact(local=dict(globals(), **locals()))
-        import code; code.interact(local=dict(globals(), **locals()))
 
-        call_seq = method_soup.find('pre', class_='syntaxbox').text #strip this?
-        #
-        # if call_seq is not None:
-        #     call_seq
+        call_seq_box = method_soup.find('pre', class_='syntaxbox') #strip this?
+        if call_seq_box is not None:
+            call_seq = call_seq_box.text
+        else:
+            call_seq = method_soup.find('pre').text
+
+        edit_box = method_soup.find('iframe')
+        # import code; code.interact(local=dict(globals(), **locals()))
+        if edit_box is not None:
+            code_page = requests.get(edit_box['src'])
+            code_soup = BeautifulSoup(code_page.content, 'html.parser')
+
+            snippet_box = code_soup.find('pre')
+            if snippet_box is not None:
+                snippet = snippet_box.text
+            else:
+                # snippet = method_soup.find('pre').text
+                example_header = method_soup.find('h2', string='Examples')
+                first_example = example_header.find_next_sibling('h3')
+                snippet = first_example.find_next_sibling('pre').text
+        else:
+            # import code; code.interact(local=dict(globals(), **locals()))
+            snippet = method_soup.find('pre').text
+
+        #*************************************************************
+        # previous code for examples
         # snippet - examples start under a H2 "Examples"
         # ends at an H2 called Polyfill
-        example_header = method_soup.find('h2', string='Examples')
-        example_h3s = example_header.find_next_siblings('h3')
-        snippet = ''
-        # hello = method_soup.find('iframe')<---this is where the editor is
-        for example in example_h3s:
-            code = example.find_next_sibling('pre').text
-            snippet = snippet + code + '\n'
-        # import code; code.interact(local=dict(globals(), **locals()))
+        # example_header = method_soup.find('h2', string='Examples')
+        # example_h3s = example_header.find_next_siblings('h3')
+        # snippet = ''
+        # for example in example_h3s:
+        #     code = example.find_next_sibling('pre').text
+        #     snippet = snippet + code + '\n'
+        #*************************************************************
         new_method = Method(name, url, call_seq, description, snippet)
-        # print(new_method.name)
+        print(new_method.name + new_method.snippet)
         methods.append(new_method)
-        # methods.append(Method(name, url, call_seq, description, snippet))
-    # the snippets are then held in a github repo
     methods
-    import code; code.interact(local=dict(globals(), **locals()))
+import code; code.interact(local=dict(globals(), **locals()))
 
 # to iterate over siblings
     # for sibling in example_header.next_siblings:
