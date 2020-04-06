3a4492682a7f23550a19f18e5b68e5bba2fd7bc8
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-06 16:49:39 -0600

    Move method definitions to main app file

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import Flask
 
+# App factory method to create instance of Flask app
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config = True)
 
@@ -10,7 +11,6 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/rosetta_dev",
         SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     )
 
     if test_config is None:
@@ -29,7 +29,31 @@ def create_app(test_config=None):
 
     return app
 
-
+# Instantiate Flask app and SQLAlchemy ORM
 app = create_app()
 db = SQLAlchemy(app)
-from .model import Language
+
+# Define models in relation to instantiated ORM
+class Language(db.Model):
+    __tablename__ = 'languages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    methods = db.relationship('Method', backref='language')
+
+    def __repr__(self):
+        return '<Language %r>' % self.name
+
+class Method(db.Model):
+    __tablename__ = 'methods'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    snippet = db.Column(db.Text)
+    syntax = db.Column(db.Text)
+    description = db.Column(db.Text)
+    docs_url = db.Column(db.String)
+    language_id = db.Column(db.Integer, db.ForeignKey('languages.id'))
+
+    def __repr__(self):
+        return '<Method %r>' % self.name
