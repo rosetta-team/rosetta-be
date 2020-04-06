c1c0b711182d09085b02d5d8ca40d25d9443d762
Alison Vermeil <alison.minton@gmail.com>
2020-04-06 12:53:43 -0600

    Wip

--- ali_scraper_spike/js_docs.py
+++ ali_scraper_spike/js_docs.py
@@ -7,7 +7,7 @@ URL = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-import code; code.interact(local=dict(globals(), **locals()))
+# import code; code.interact(local=dict(globals(), **locals()))
 methods_div = soup.find('div', class_="quick-links")
 methods_list = methods_div.find('ol')
 method_links = methods_list.find_all('a')
@@ -17,12 +17,36 @@ method_links = methods_list.find_all('a')
 methods = []
 for link in method_links[6:45]:
     description = link['title']
-    name = link.find('code').text
-    call_seq = '' #this is in a different part of the page
-    snippet = '' #most methods would inolve actually going to another page to get a snippet
-    # the snippets are then held in a github repo
     if "deprecated" not in description:
-        # print(link.find('code').text)
-        # print(link['title'])
-        methods.append(Method(name, call_seq, description, snippet))
+        name = link.find('code').text
+        url = 'https://developer.mozilla.org/' + link['href']
+        method_page = requests.get(url) #have to make another request to the new site
+        method_soup = BeautifulSoup(method_page.content, 'html.parser')
+        # import code; code.interact(local=dict(globals(), **locals()))
+        import code; code.interact(local=dict(globals(), **locals()))
+
+        call_seq = method_soup.find('pre', class_='syntaxbox').text #strip this?
+        #
+        # if call_seq is not None:
+        #     call_seq
+        # snippet - examples start under a H2 "Examples"
+        # ends at an H2 called Polyfill
+        example_header = method_soup.find('h2', string='Examples')
+        example_h3s = example_header.find_next_siblings('h3')
+        snippet = ''
+        # hello = method_soup.find('iframe')<---this is where the editor is
+        for example in example_h3s:
+            code = example.find_next_sibling('pre').text
+            snippet = snippet + code + '\n'
+        # import code; code.interact(local=dict(globals(), **locals()))
+        new_method = Method(name, url, call_seq, description, snippet)
+        # print(new_method.name)
+        methods.append(new_method)
+        # methods.append(Method(name, url, call_seq, description, snippet))
+    # the snippets are then held in a github repo
     methods
+    import code; code.interact(local=dict(globals(), **locals()))
+
+# to iterate over siblings
+    # for sibling in example_header.next_siblings:
+    #     print(repr(sibling))
