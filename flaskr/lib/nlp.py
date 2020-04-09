098dcab32a0ec8f36db2d777760820af4a7d3e1e
msimon42 <msimon@uab.edu>
2020-04-08 23:18:06 -0600

    Add compare method

--- flaskr/lib/nlp.py
+++ flaskr/lib/nlp.py
@@ -1,7 +1,11 @@
 import spacy
 
 class DescriptionComparer:
-    def __init__(description1, description2):
+    def __init__():
         self.nlp = spacy.load('en_core_web_lg')
-        self.doc1 = description1
-        self.doc2 = description2
+
+    def compare(description1, description2):
+        doc1 = self.nlp(description1)
+        doc2 = self.nlp(description2)
+
+        return doc1.similarity(doc2)
