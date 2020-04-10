fb63618d026c78d989a104c5747152055645db5c
msimon42 <msimon@uab.edu>
2020-04-09 21:47:45 -0600

    Add script to populate method results
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- flaskr/relevancy_rating_generator.py
+++ flaskr/relevancy_rating_generator.py
@@ -18,5 +18,16 @@ def search_result_generator():
         db.session.add(search_result)
         db.session.commit()
 
+def generate_method_results():
+    search_results = SearchResult.query.all()
+    js_methods = Method.query.filter_by(language_id=2)
+    ruby_methods = Method.query.filter_by(language_id=1)
+    fake_array = []
 
-     
+    for result in search_results:
+        methods = Method.query.filter_by(language_id=result.target_language_id)
+        for method in methods:
+            method_result = MethodResult(method_id=method.id, search_result_id=result.id)
+            fake_array.append(method_result)
+            db.session.add(method_result)
+            db.session.commit()
