cfa22176745b49d9a91508d4f04b2cc3bc73dbfa
msimon42 <msimon@uab.edu>
2020-04-11 16:34:54 -0600

    Add special character converter

--- flaskr/lib/description_comparer.py
+++ flaskr/lib/description_comparer.py
@@ -4,6 +4,25 @@ class DescriptionComparer:
     def __init__(self):
         self.nlp = spacy.load('en_core_web_lg')
 
+    def convert_special_chars(self, word):
+        conversion_dict = {
+            '<<':'push',
+            '&':'intersection',
+            '+':'concat',
+            '-':'splice',
+            '*':'join',
+            '<=>':'find',
+            '==':'equals',
+            '[]':'find'
+        }
+
+        special_symbols = conversion_dict.keys()
+        for symbol in special_symbols:
+            if symbol in word:
+                word.replace(symbol, conversion_dict[symbol])
+
+        return word
+
 
     def compare(self, description1, description2):
         doc1 = self.nlp(description1)
