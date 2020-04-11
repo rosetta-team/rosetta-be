f6f197787957ed6b412d3afa841cbb533673b92d
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-11 15:45:04 -0600

    Remove unused conditional instructions regarding config.py
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Define build job on CircleCI config file
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Combine build and build-and-test jobs and correct typo
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Reinstate orb configuration
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Add step to install postgresql client to allow psql usage
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Add PGHOST environmental variable to CircleCI config file
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Remove POSTGRES_DB environement variable declaration and rename DB
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Declare postgres_user environmental variable
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Move postgres_user variable to python docker image
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Change username to root and remove extraneous pieces of database url
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Refine database_url and environment vars to reflect examples
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Relocate postgresql env vars to postgresql docker image section
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Change username on table creation line to postgres
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Add command to sudo into postgres user before creating database
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Revert to previous working yml file structure and add DB setup step
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Correct indentation error
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Add PG_HOST environmental variable back into file
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>
    
    Revert to last working CircleCI config
    
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- flaskr/app.py
+++ flaskr/app.py
@@ -20,11 +20,6 @@ def create_app(test_config=None):
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
     try:
         os.makedirs(app.instance_path)
     except OSError:
