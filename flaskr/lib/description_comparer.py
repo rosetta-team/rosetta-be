a4b7b89373af470a51bc5b9ced0011fe468d102d
msimon42 <msimon@uab.edu>
2020-04-11 15:52:42 -0600

    test

--- flaskr/lib/description_comparer.py
+++ flaskr/lib/description_comparer.py
@@ -4,14 +4,10 @@ class DescriptionComparer:
     def __init__(self):
         self.nlp = spacy.load('en_core_web_lg')
 
+
     def compare(self, description1, description2):
         doc1 = self.nlp(description1)
         doc2 = self.nlp(description2)
 
-        print(doc1) 
-        print(description1)
-        print(doc2)
-        print(description2)
-        print(doc1.similarity(doc2))
-        print("--------")
         return doc1.similarity(doc2)
+#test
