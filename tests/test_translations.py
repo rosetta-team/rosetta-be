3283e11d7c39dbf39bc457fbab5a9575d030b28b
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-04-11 15:36:03 -0600

    Refactor test of translations GraphQL query to be data-independent
    Co-Authored-By: Alison Vermeil <alison.minton@gmail.com>

--- tests/test_translations.py
+++ tests/test_translations.py
@@ -1,33 +1,23 @@
-import json
 import pytest
-from .conftest import client
-# from graphene.test import Client
+from graphene.test import Client
 from flaskr.app import schema
 
 def test_translations(client):
-    # print('hello world')
     query = """
-    {
-        translations(targetLanguageId: 2,methodId: 58) {
-            name
-            snippet
-            syntax
-            description
-            docsUrl
-            methodResults {
-                edges {
-                    node {
-                        relevanceRating
-                    }
-                }
-            }
-        }
+    { translations(targetLanguageId: 2, methodId: 58)
+        {
+          	relevanceRating
+          	method {
+          	  id
+              name
+              description
+              syntax
+              snippet
+              docsUrl
+          	}
+    	}
     }
     """
-    # client = Client(schema)
-    # response = client.execute(query)
-    # import code; code.interact(local=locals())
-    response = client.post('/graphql', query)
-    print(response)
-    # print('I am here!')
-    # assert response == {
+    client = Client(schema)
+    response = client.execute(query)
+    assert response['data']
