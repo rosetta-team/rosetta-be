0e84627d3bf42713b7aff2397ad604171cc30e65
msimon42 <msimon@uab.edu>
2020-04-12 13:24:09 -0600

    Fix test
    Co-Authored-By: Joshua O'bannon <jtobannon@gmail.com>

--- tests/test_description_comparer.py
+++ tests/test_description_comparer.py
@@ -10,7 +10,7 @@ class TestDescriptionComparer:
         desc1 = 'Set Intersection — Returns a new array containing unique elements common to the two arrays. The order is preserved from the original array. It compares elements using their hash and eql? methods for efficiency. See also'
         desc2 = 'The Array.of() method creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments.'
 
-        assert self.comparer.compare(desc1, desc2) == 0.9098949965688894
+        assert self.comparer.compare(desc1, desc2) == 0.9049812069066979
 
     def test_break_camelcase(self):
         text = 'isArray'
